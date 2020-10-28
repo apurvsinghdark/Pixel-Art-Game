83d0f30afaeeb80cb3d6131afa759c13c4785d88
Apurv singh <apurvsingh9713@gmail.com>
2020-10-28 20:43:13 +0530

    Add. UI

---
+++ Assets/Scripts/UI/UiManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+    public void OnStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
